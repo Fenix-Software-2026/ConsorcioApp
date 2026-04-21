97107516dfb2a1962b6bfe5282cdb2f97c341720
Ivan Moreno Rivero <iv4n.m0reno@gmail.com>
2026-04-20 21:17:48 -0300

    correcciones de version de python para trabajar con la misma version de Django

--- back/myapp/views.py
+++ back/myapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Movimiento
+from django.http import JsonResponse
 
 class TestView(APIView):
     def get(self, request):
@@ -10,3 +11,6 @@ class MovimientoView(APIView):
     def get(self, request):
         data = list(Movimiento.objects.values())
         return Response(data)
+    
+def home(request):
+    return JsonResponse({"mensaje": "API ConsorcioApp funcionando"})
