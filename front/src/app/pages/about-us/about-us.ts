3e177251fb3f6c117a9d0193f181be30fd191df9
NahuelC55 <nahuucufre@gmail.com>
2026-05-09 21:03:38 -0300

    mod: modificación de tarjetas de miembros (borrador)

--- front/src/app/pages/about-us/about-us.ts
+++ front/src/app/pages/about-us/about-us.ts
@@ -13,16 +13,37 @@ export class AboutUs implements OnInit {
   equipo = [
     {
       nombre: 'Nahuel Santiago Cufre',
-      rol: 'Front-end Developer',
-      bio: 'Estudiante de desarrollo web del Instituto Superior Politécnico de Córdoba.',
-      foto: 'assets/img/nahuel.jpg' 
+      rol: 'Scrum Master | Front-end Developer',
+      bio: 'Desarrollador front-end y Scrum Master del proyecto',
+      foto: '/profile-silhouette.png' 
     },
     {
-      nombre: 'dev2', 
-      rol: 'rol de el otro desarrollador',
-      bio: 'bio de el otro desarrollador',
-      foto: 'assets/img/dev2.jpg'
-    }
+      nombre: 'Ivan Ignacio Moreno Rivero', 
+      rol: 'Full-Stack Developer',
+      bio: 'Desarrollador Full-Stack del proyecto',
+      foto: '/profile-silhouette.png'
+    },
+    {
+      nombre: 'Luis Gastón Alonso',
+      rol: 'Back-end developer | Database Administrator',
+      bio: 'Desarrollador back-end y administrador de base de datos del proyecto',
+      foto: '/profile-silhouette.png' 
+    },
+       {
+      nombre: 'Luis Gerardo Catalas',
+      rol: 'Back-end developer | Configuration Manager',
+      bio: 'Desarrollador back-end y gestión de las versiones del código',
+      foto: '/profile-silhouette.png' 
+    },
+          {
+      nombre: 'Veronica Cecilia Vargas',
+      rol: 'Technical Writer | Product Owner',
+      bio: 'Redactora técnica de documentación y responsable de maximizar el valor del producto',
+      foto: '/profile-silhouette.png' 
+    },
+
+
+    
   ];
 
   ngOnInit() {
