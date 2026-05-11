5c9f662e06c19270ce6420256f02d44636b28666
NahuelC55 <nahuucufre@gmail.com>
2026-05-10 21:09:42 -0300

    feat: modificar template de landing. agregar reactive form para contacto

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Landing } from './pages/landing/landing';
 import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Landing, Footer],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
