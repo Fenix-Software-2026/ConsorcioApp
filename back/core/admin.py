be7611c4a7c9012f23070dafb5a46808622d1365
gastón_Alonso() <tonga88@live.com.ar>
2026-05-24 13:36:04 -0300

    feat: customize admin panel for custom user model

--- back/core/admin.py
+++ back/core/admin.py
@@ -1,3 +1,40 @@
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core.models import Usuario
 
 # Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Información adicional',
+            {
+                'fields': (
+                    'rol',
+                    'unidad',
+                    'residente_actual',
+                )
+            }
+        ),
+    ) # type: ignore
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            'Información adicional',
+            {
+                'fields': (
+                    'rol',
+                    'unidad',
+                    'residente_actual',
+                )
+            }
+        ),
+    )
+    list_display = (
+        'username',
+        'email',
+        'rol',
+        'unidad',
+        'is_staff',
+    )
