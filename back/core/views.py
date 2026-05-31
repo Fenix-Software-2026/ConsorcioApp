f68d62366ca565451a90ca88d2f41be002a91d6d
gastón_Alonso() <tonga88@live.com.ar>
2026-05-31 19:26:40 -0300

    feat(backend): add custom permission classes to viewsets for role-based access

--- back/core/views.py
+++ back/core/views.py
@@ -1,21 +1,24 @@
 from rest_framework import viewsets
 
-from core.permissions import EsAdminConsorcio
+from core.permissions import EsAdminConsorcio, EsResidente
 from .models import Reclamo, Comunicado, Usuario, Unidad
 from .serializers import ReclamoSerializer, ComunicadoSerializer, UsuarioSerializer, UnidadSerializer
 
 class ReclamoViewSet(viewsets.ModelViewSet):
-    permission_classes = [EsAdminConsorcio]
+    permission_classes = [EsResidente]
     queryset = Reclamo.objects.all()
     serializer_class = ReclamoSerializer
 
 class ComunicadoViewSet(viewsets.ModelViewSet):
+    permission_classes = [EsAdminConsorcio]
     queryset = Comunicado.objects.all()
     serializer_class = ComunicadoSerializer
     
 class UsuarioViewSet(viewsets.ModelViewSet):
+    permission_classes = [EsAdminConsorcio]
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 class UnidadViewSet(viewsets.ModelViewSet):
+    permission_classes = [EsAdminConsorcio]
     queryset = Unidad.objects.all()
     serializer_class = UnidadSerializer
