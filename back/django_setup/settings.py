e226f10b70e0ad5b8cbc84227a8bb80a2e8f503d
gastón_Alonso() <tonga88@live.com.ar>
2026-09-24 01:57:11 -0300

    se agrego whitenoise para tener estilos en /admin

--- back/django_setup/settings.py
+++ back/django_setup/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  
 AUTH_USER_MODEL = 'core.Usuario'
